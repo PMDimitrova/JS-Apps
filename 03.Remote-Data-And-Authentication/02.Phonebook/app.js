b4746f4b3ea06f6c7fa02bac9804f9f4cb7d82e0
PMDimitrova <70662553+PMDimitrova@users.noreply.github.com>
2021-03-27 19:18:46 +0200

    HW

--- 03.Remote-Data-And-Authentication/02.Phonebook/app.js
+++ 03.Remote-Data-And-Authentication/02.Phonebook/app.js
@@ -1,5 +1,70 @@
 function attachEvents() {
-    console.log('TODO...');
+    /*
+    +onclick load-btn get all the phone-nums from the server
+         +display them in <li> in the required format + delete-btn
+         -attach event listener to this btn and create delete-function
+             -it should send 'delete'-request to the server
+             -the delete-request should return a response in a specific format
+     -onclick create-btn
+         -> post-request to the server
+         -> clear the input fields
+         -> reload phonebook (repeat the function from above)
+     */
+
+    document.getElementById('btnLoad').addEventListener('click', fetchDataFromServer);
+
+    document.getElementById('btnCreate').addEventListener('click', createNewEntry);
+}
+
+async function fetchDataFromServer() {
+    const response = await fetch('http://localhost:3030/jsonstore/phonebook');
+    const data = await response.json();
+    document.getElementById('phonebook').innerHTML = '';
+
+    Object.entries(data).forEach(element => createAttachPhoneNr(element));
+
+    Array.from(document.getElementsByClassName('btnDelete')).forEach(el => {
+        el.addEventListener('click', deleteEntry);
+    });
+
+}
+
+function createAttachPhoneNr(element) {
+    let id = element[0];
+    let human = element[1];
+    let htmlElement = `<li>${human.person}: ${human.phone} <input type="hidden" id="${id}"> <button class="btnDelete">Delete</button> </li>`;
+    document.getElementById('phonebook').innerHTML += htmlElement;
+}
+
+async function deleteEntry(event) {
+    const idToDelete = event.target.parentNode.querySelector('input').id;
+
+    const response = await fetch('http://localhost:3030/jsonstore/phonebook/' + idToDelete,{
+        method: 'delete',
+    });
+
+    fetchDataFromServer();
+}
+
+async function createNewEntry(event){
+    let name = document.getElementById('person');
+    let phoneNum = document.getElementById('phone');
+
+    const personToAdd ={
+        person: name.value,
+        phone: phoneNum.value
+    }
+
+    const result = await fetch('http://localhost:3030/jsonstore/phonebook', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(personToAdd)
+    });
+
+    name.value = '';
+    phoneNum.value = '';
+
+    fetchDataFromServer();
 }
 
 attachEvents();
--- 03.Remote-Data-And-Authentication/03.Students/app.js
+++ 03.Remote-Data-And-Authentication/03.Students/app.js
@@ -1,1 +1,79 @@
-console.log('TODO...');
+// window.addEventListener('load', mainFunction);
+
+function mainFunction(){
+    fetchDataFromServer();
+    const form = document.getElementById('form');
+    form.addEventListener('submit', event =>{
+        event.preventDefault();
+        const formData = new FormData(form);
+
+        const firstName = formData.get('firstName');
+        const lastName = formData.get('lastName');
+        const facultyNumber = formData.get('facultyNumber');
+        const grade = formData.get('grade');
+
+        try{
+            checkIfEmpty([firstName, lastName, facultyNumber, grade]);
+
+            const student = {
+                firstName: firstName,
+                lastName: lastName,
+                facultyNumber: facultyNumber,
+                grade: grade,
+            };
+
+            displayStudent(student);
+            addStudentToDB(student);
+
+            event.target.reset();
+        }catch (error){
+            alert(error.message);
+        }
+
+    });
+}
+
+async function fetchDataFromServer(){
+    const response = await fetch('http://localhost:3030/jsonstore/collections/students');
+    const data = await response.json();
+
+    Object.entries(data).forEach(element => displayStudent(element[1]));
+    // console.log(data);
+}
+
+function checkIfEmpty(inputArray){
+    inputArray.forEach(input => {
+        if (input.trim() === ''){
+            throw new Error('All fields are required!');
+        }
+    });
+}
+
+function displayStudent(person){
+    let {firstName} = person;
+    let {lastName} = person;
+    let {facultyNumber} = person;
+    let {grade} = person;
+
+    let studentElement = `<tr><td>${firstName}</td><td>${lastName}</td><td>${facultyNumber}</td><td>${grade}</td></tr>`;
+    document.getElementById('tableBody').innerHTML += studentElement;
+}
+
+async function addStudentToDB(student){
+    const response = await fetch('http://localhost:3030/jsonstore/collections/students',{
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(student)
+    });
+}
+
+async function request(url, options) {
+    const response = await fetch(url, options);
+    if (response.ok != true) {
+        const error = await response.json();
+        alert(error.message);
+        throw new Error(error.message);
+    }
+    const data = await response.json();
+    return data;
+}
